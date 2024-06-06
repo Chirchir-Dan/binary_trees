7e0156998eb0da72063869488f28ffcb3ebb083d
Chirchir-Dan <1danchirchir@gmail.com>
2024-06-06 15:50:31 +0300

    Wrote 111 insert in BST

--- binary_trees.h
+++ binary_trees.h
@@ -20,6 +20,9 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
 /* Function prototype */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
@@ -48,5 +51,6 @@ int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
+bst_t *bst_insert(bst_t **tree, int value);
 
 #endif /* BINARY_TREES_H */
